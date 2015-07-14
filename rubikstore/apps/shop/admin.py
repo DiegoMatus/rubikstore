975ebc7e5e89b96d127fe5893b1c3209df4d4c25
diegoMatus <cube.di.rubik@gmail.com>
2015-07-13 23:02:06 -0500

    add models to admin

--- rubikstore/apps/shop/admin.py
+++ rubikstore/apps/shop/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from models import *
 # Register your models here.
+
+admin.site.register(Category)
+admin.site.register(Brand)
+admin.site.register(Base)
+admin.site.register(Product)
+admin.site.register(City)
+admin.site.register(Country)
+admin.site.register(State)
+#admin.site.register(Order)
+#admin.site.register(Payment)
+admin.site.register(Inventory)
+#admin.site.register(Comment)
+admin.site.register(ImageProduct)
+#admin.site.register(Address)
