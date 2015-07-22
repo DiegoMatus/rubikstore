5454312097b89d886b1682f4f7b3751fd7412290
diegomatus <cube.di.rubik@gmail.com>
2015-07-22 13:20:14 -0500

    static modified

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, url
 
 urlpatterns = patterns('apps.shop.views',
-                       url(r'^$', 'index_view', name='index'),
+                       url(r'^$', 							'index_view', 			name='index'),
+                       url(r'^(?P<filter>\w+)/', 			'shop_view', 			name='shop'),
+                       url(r'^details/(?P<product>\w+)/', 	'single_product_view', 	name='single_product'),
+                       url(r'^shoppingcart/', 				'cart_view', 			name='cart'),
+                       url(r'^checkout/', 					'checkout_view', 		name='checkout'),
                        )
