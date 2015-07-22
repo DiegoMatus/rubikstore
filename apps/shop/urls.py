8605125ebd37e07910453aa22c0bd50a749cc767
diegomatus <cube.di.rubik@gmail.com>
2015-07-22 18:47:28 -0500

    add templates, urls and views

--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -1,9 +1,34 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, url, include
+
+#Used to remove redundancy from URLconfs where a single pattern prefix is used repeatedly (Require from . import views)
+#url(r'^checkout/', include([
+#	 		url(r'^cart/$', 			views.shoppingcart_view,	name='shoppingcart'),
+#			url(r'^shipping/$', 		views.shipping_view, 		name='shipping'),
+#])),
+
 
 urlpatterns = patterns('apps.shop.views',
-                       url(r'^$', 							'index_view', 			name='index'),
-                       url(r'^(?P<filter>\w+)/', 			'shop_view', 			name='shop'),
-                       url(r'^details/(?P<product>\w+)/', 	'single_product_view', 	name='single_product'),
-                       url(r'^shoppingcart/', 				'cart_view', 			name='cart'),
-                       url(r'^checkout/', 					'checkout_view', 		name='checkout'),
+                       url(r'^$', 								'index_view', 			name='index'),
+                       url(r'^types/$', 						'types_view', 			name='types'),
+                       url(r'^types/(?P<type>\w+)/$', 			'types_view',			name='types'),
+                       url(r'^brands/$', 						'brands_view',		 	name='brands'),
+                       url(r'^brands/(?P<brand>\w+)/$', 		'brands_view', 			name='brands'),
+                       url(r'^accessories/$',					'accessories_view', 	name='accessories'),
+                       url(r'^accessories/(?P<accessory>\w+)/$','accessories_view',		name='accessories'),
+                       url(r'^details/(?P<product>\w+)/$', 		'details_view',		 	name='details'),
+                       url(r'^checkout/cart/$', 				'shoppingcart_view',	name='shoppingcart'),
+                       url(r'^checkout/shipping/$',				'shipping_view', 		name='shipping'),
+                       url(r'^contact/$',						'contact_view', 		name='contact'),
                        )
+
+#urlpatterns = patterns('apps.accounts.views',
+#                       url(r'^$', 					'account_view', 		name="account"),
+#                       url(r'^login/$', 			'login_view', 			name="login"),
+#                       url(r'^register/$', 			'register_view', 		name="register"),
+#                       url(r'^wishlist/$', 			'wishlist_view', 		name="wishlist"),
+#                       url(r'^logout/$', 			'logout_view', 			name="logout"),
+#                       url(r'^forgot_password/$', 	'forgot_password_view', name="forgot_password"),
+#                       url(r'^edit/$', 				'edit_view', 			name="edit"),
+#                       url(r'^credit_cards/$', 		'credit_cards_view', 		name="credit_cards"),
+#                       url(r'^orders/$', 			'orders_view', 			name="orders"),
+#                       )
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,6 +1,27 @@
-from django.shortcuts import render, render_to_response, RequestContext
-
+from django.shortcuts import render, render_to_response
+from django.template import RequestContext
 
 # Create your views here.
 def index_view(request):
     return render_to_response('index.html', context=RequestContext(request))
+
+def types_view(request, type=''):
+	return render_to_response('shop.html', context=RequestContext(request))
+
+def brands_view(request, brand=''):
+	return render_to_response('shop.html', context=RequestContext(request))
+
+def accessories_view(request, accessory=''):
+	return render_to_response('shop.html', context=RequestContext(request))
+
+def details_view(request, product):
+	return render_to_response('details.html', context=RequestContext(request))
+
+def shoppingcart_view(request):
+	return render_to_response('shoppingcart.html', context=RequestContext(request))
+
+def shipping_view(request):
+	return render_to_response('shipping.html', context=RequestContext(request))
+
+def contact_view(request):
+	return render_to_response('contact.html', context=RequestContext(request))
