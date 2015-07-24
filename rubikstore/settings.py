dd7c5c96a79d39b742262974498a8b4483505aa3
Daniel García Alvarado <danigarciaalva@hotmail.com>
2015-07-24 00:32:17 -0500

    fix

--- rubikstore/settings.py
+++ rubikstore/settings.py
@@ -36,19 +36,13 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
-                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
-                # list if you haven't customized them:
                 'django.contrib.auth.context_processors.auth',
                 'django.template.context_processors.debug',
                 'django.template.context_processors.i18n',
                 'django.template.context_processors.media',
                 'django.template.context_processors.static',
                 'django.template.context_processors.tz',
-                'django.contrib.messages.context_processors.messages',
-                # `allauth` needs this from django
                 'django.template.context_processors.request',
-
-                # `allauth` specific context processors
                 'allauth.account.context_processors.account',
                 'allauth.socialaccount.context_processors.socialaccount',
             ],
